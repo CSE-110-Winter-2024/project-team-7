a96ec207d5babf5d35ef5711854482c35e4e37c6
pvijay03 <pvijay@ucsd.edu>
2024-02-18 14:32:37 -0800

    #31: just some reordering in MainActivity

--- app/src/main/java/edu/ucsd/cse110/successorator/SuccessoratorApplication.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/SuccessoratorApplication.java
@@ -1,7 +1,45 @@
 package edu.ucsd.cse110.successorator;
 
-import android.app.Application;
+import androidx.room.Room;
 
-public class SuccessoratorApplication extends Application {
+import edu.ucsd.cse110.successorator.data.db.DateDatabase;
+import edu.ucsd.cse110.successorator.data.db.GoalDatabase;
+import edu.ucsd.cse110.successorator.data.db.RoomDateStorage;
+import edu.ucsd.cse110.successorator.data.db.RoomGoalLists;
 
+public class Successorator {
+    private RoomDateStorage storedDate;
+
+    public void initializeDatabases(MainActivity mainActivity) {
+        GoalDatabase goalDatabase = Room.databaseBuilder(
+                mainActivity.getApplicationContext(),
+                GoalDatabase.class,
+                "goals-database"
+        ).allowMainThreadQueries().build();
+
+        mainActivity.todoList = new RoomGoalLists(goalDatabase.goalDao());
+
+        DateDatabase dateDatabase = Room.databaseBuilder(
+                mainActivity.getApplicationContext(),
+                DateDatabase.class,
+                "date-database"
+        ).allowMainThreadQueries().build();
+
+        storedDate = new RoomDateStorage(dateDatabase.dateDao());
+
+        checkFirstRun(mainActivity);
+    }
+
+    private void checkFirstRun(MainActivity mainActivity) {
+        android.content.SharedPreferences sharedPreferences = mainActivity.getSharedPreferences("goals", MainActivity.MODE_PRIVATE);
+        boolean isFirstRun = sharedPreferences.getBoolean("isFirstRun", true);
+
+        if (isFirstRun) {
+            storedDate.replace(mainActivity.currentDate);
+            sharedPreferences.edit().putBoolean("isFirstRun", false).apply();
+        } else if (!mainActivity.currentDate.getFormattedDate().equals(storedDate.formattedDate())) {
+            mainActivity.currentDate.updateDate(storedDate.formattedDate());
+            storedDate.replace(mainActivity.currentDate);
+        }
+    }
 }
